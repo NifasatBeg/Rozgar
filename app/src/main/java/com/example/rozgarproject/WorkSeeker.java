ebad458adfa814a6e4e63ff7d0217bc5820b7c77
Rohan <rohanbondre2001@gmail.com>
2021-10-19 19:01:59 +0530

    textview added

--- app/src/main/java/com/example/rozgarproject/WorkSeeker.java
+++ app/src/main/java/com/example/rozgarproject/WorkSeeker.java
@@ -2,13 +2,33 @@ package com.example.rozgarproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
 
-public class WorkSeeker extends AppCompatActivity {
+public class WorkSeeker extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_work_seeker);
+        setContentView(R.layout.activity_work_provider);
+        Button AppliedJobs = (Button) findViewById(R.id.AppliedJobs);
+        Button SearchJob = (Button) findViewById(R.id.Search_Job);
+        AppliedJobs.setOnClickListener(WorkSeeker.this);
+        SearchJob.setOnClickListener(WorkSeeker.this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.AppliedJobs:
+                startActivity(new Intent(WorkSeeker.this,PersonalJobsApplied.class));
+                break;
+            case R.id.Search_Job:
+                startActivity(new Intent(WorkSeeker.this,Openings.class));
+                break;
+        }
     }
 }
