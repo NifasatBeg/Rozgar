3b8fee8564b0d977281c6da1ca80fac0fbe2c332
Nifasat <mohd.beg@walchandsangli.ac.in>
2021-11-11 10:03:46 +0530

    comut

--- app/src/main/java/com/example/rozgarproject/WorkSeeker.java
+++ app/src/main/java/com/example/rozgarproject/WorkSeeker.java
@@ -16,7 +16,6 @@ public class WorkSeeker extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_work_seeker);
         Button AppliedJobs = (Button) findViewById(R.id.AppliedJobs);
         Button SearchJob = (Button) findViewById(R.id.Search_Job);
-        AppliedJobs.setOnClickListener(WorkSeeker.this);
     }
 
     @Override
